e96faeef5871dadfa26e204e89190ff2e3e65dbb
Adam Hallberg <adamdavidhallberg@gmail.com>
2022-10-15 15:56:01 +0200

    bg image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
 content: ["./dist/*.{html,js}"],
 theme: {
-  extend: {},
+  extend: {
+    fontFamily: {
+      'sanity': ['karlaregular', 'sans-serif'],
+    },
+    keyframes: {
+      progress: {
+        "0%": {width: "0%"},
+        "100%": {width: "100%"}
+      }
+    },
+    animation: {
+      "progressbar": "progress 1200ms linear"
+  }
+  },
 },
 plugins: [],
 }

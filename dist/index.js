92582e3a0a62ecc0d385bf79be802d48b94bd2c9
Adam Hallberg <adamdavidhallberg@gmail.com>
2022-10-15 23:18:42 +0200

    sample done

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,10 @@
 const burger = document.getElementById("burger");
 const sidebar = document.getElementById("sidebar");
 const close = document.getElementById("close");
-burger.classList.toggle("hidden");
+const dropdown = document.getElementById("dropdown");
+const travel = document.getElementById("travel");
+const closeInfo = document.getElementById("close-info");
+const travelBar = document.getElementById("travel-bar");
 
 burger.addEventListener('click', () => {
   sidebar.classList.toggle("hidden");
@@ -11,4 +14,14 @@ burger.addEventListener('click', () => {
 close.addEventListener('click', () => {
   sidebar.classList.toggle("hidden");
   burger.classList.toggle("hidden");
+});
+
+travel.addEventListener('click', () => {
+  dropdown.classList.toggle("hidden");
+  travelBar.classList.toggle('w-[100%]')
+});
+
+closeInfo.addEventListener('click', () => {
+  dropdown.classList.toggle("hidden");
+  travelBar.classList.toggle('w-[100%]')
 });
